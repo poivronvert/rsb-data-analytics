bdda180170cef50b07323cd8d5d9b3ef05fb36ba
thisishugow <59921505+thisishugow@users.noreply.github.com>
2023-12-11 16:26:35 +0100

    init home page

--- rsb_data_analytics/app.py
+++ rsb_data_analytics/app.py
@@ -2,6 +2,8 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+from rsb_data_analytics.routers.router import router
 
 app = FastAPI()
 
@@ -15,3 +17,6 @@ app.add_middleware(
 
 LIB_ROOT:os.PathLike = os.path.dirname(os.path.realpath(__file__))
 app.mount("/static", StaticFiles(directory=os.path.join(LIB_ROOT, 'static')), name="static")
+
+app.include_router(router)
+logging.info('Start listen on: http://localhost:21116')
